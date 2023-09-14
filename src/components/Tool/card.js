67085d1d3366b8901eef03a09d531de3c30aafac
vijay kumar <64150328+vijay6781@users.noreply.github.com>
2023-09-14 23:56:30 +0530

    added files

--- src/components/Tool/card.js
+++ src/components/Tool/card.js
@@ -16,7 +16,7 @@ const Card = ({ title, description, imageUrl }) => {
         </div>
 
         <div className="mt-auto"> {/* Use mt-auto for pushing the button to the bottom */}
-          <div className="ml-11">
+          <div className="ml-12">
             <Button text="Apply" url="/apply" />
           </div>
         </div>
