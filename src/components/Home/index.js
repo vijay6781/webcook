75fab4e7ed2c0d25321b1a9569b2b2ccd80e7bb2
vijay kumar <64150328+vijay6781@users.noreply.github.com>
2023-09-17 13:03:36 +0530

    add cards

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,7 +9,7 @@ const Home = () => {
     
       
      <div>
-      <Banner imageSrc={bannerImage} text="Make Website for your bussiness" text1="with low price" />
+      <Banner imageSrc={bannerImage} text="Make Website for your bussiness" text1="make your own brand at low price" />
       <DisplayCard/>
       <Card/>
       
