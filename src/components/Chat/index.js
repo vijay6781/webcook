3244f5bea810fe3eb5155005bb8bf3993ec17c0b
vijay kumar <64150328+vijay6781@users.noreply.github.com>
2023-09-18 22:50:42 +0530

    add chat

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -2,24 +2,65 @@ import React, { useState, useEffect } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-import { FaTrash, FaThumbsUp, FaThumbsDown } from 'react-icons/fa';
+import 'firebase/compat/database';
+import { FaTrash, FaHeart } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import './Chat.css'; // Add your CSS file for additional styling
 
 const Chat = () => {
   const [user, setUser] = useState(null);
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
 
+  const handleLikeMessage = (msgId) => {
+    const db = firebase.firestore();
+    const messagesRef = db.collection('messages');
+    const messageRef = messagesRef.doc(msgId);
+
+    messageRef.get().then((doc) => {
+      if (doc.exists) {
+        const currentLikes = doc.data().likes || [];
+        if (!currentLikes.includes(user.uid)) {
+          currentLikes.push(user.uid);
+        } else {
+          currentLikes.splice(currentLikes.indexOf(user.uid), 1); // Toggle like
+        }
+        messageRef.update({
+          likes: currentLikes,
+        });
+      }
+    });
+  };
+
+  const handleDeleteMessage = async (msg) => {
+    try {
+      const db = firebase.firestore();
+      const messagesRef = db.collection('messages');
+      await messagesRef.doc(msg.id).delete();
+    } catch (error) {
+      console.error('Error deleting message:', error);
+    }
+  };
+
+  useEffect(() => {
+    const db = firebase.firestore();
+    const messagesRef = db.collection('messages');
+
+    const unsubscribe = messagesRef.orderBy('timestamp').onSnapshot((snapshot) => {
+      const newMessages = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setMessages(newMessages);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   useEffect(() => {
     const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         setUser(user);
-        const db = firebase.firestore();
-        const messagesRef = db.collection('messages');
-        messagesRef.orderBy('timestamp').onSnapshot((snapshot) => {
-          const newMessages = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-          setMessages(newMessages);
-        });
       } else {
         setUser(null);
       }
@@ -38,106 +79,76 @@ const Chat = () => {
         firstName: user.displayName.split(' ')[0],
         photoURL: user.photoURL,
         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-        likes: [] // Initialize likes array for new messages
+        likes: [], // Initialize likes as an empty array
       });
       setMessage('');
     }
   };
 
-  const handleDeleteMessage = async (msg) => {
-    try {
-      const db = firebase.firestore();
-      const messagesRef = db.collection('messages');
-      await messagesRef.doc(msg.id).delete();
-    } catch (error) {
-      console.error('Error deleting message:', error);
-    }
-  };
-
-  const handleLikeMessage = async (msg) => {
-    try {
-      const db = firebase.firestore();
-      const messagesRef = db.collection('messages');
-      const msgDoc = await messagesRef.doc(msg.id).get();
-
-      if (msgDoc.exists) {
-        const likes = msgDoc.data().likes || [];
-        const userHasLiked = likes.includes(user.uid);
-
-        if (userHasLiked) {
-          likes.splice(likes.indexOf(user.uid), 1); // Remove user's like
-        } else {
-          likes.push(user.uid); // Add user's like
-        }
-
-        await messagesRef.doc(msg.id).update({ likes });
-      }
-    } catch (error) {
-      console.error('Error updating likes:', error);
-    }
-  };
-
   return (
-    <div className="max-w-lg mx-auto p-4 bg-white rounded shadow">
+    <div className="max-w-lg mx-auto p-4 bg-white rounded shadow chat-container">
       {user ? (
         <>
-          <ul className="mb-4">
+          <ul className="message-list">
             {messages.map((msg) => (
-              <li key={msg.id} className="mb-2">
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center">
+              <li key={msg.id} className="message-item">
+                <div className="message-content">
+                  <div className="user-info">
                     <img
                       src={msg.photoURL}
                       alt={msg.firstName}
-                      className="w-8 h-8 rounded-full mr-2"
+                      className="user-avatar"
                     />
-                    <span className="bg-blue-100 p-2 rounded">{msg.text}</span>
-                    {msg.timestamp && (
-                      <div className="text-right ml-2 text-xs text-gray-500">
-                        {new Date(msg.timestamp.toDate()).toLocaleTimeString()}
-                      </div>
-                    )}
+                    <span className="user-name">{msg.firstName}</span>
                   </div>
-                  <div className="flex items-center">
-                    <button
-                      onClick={() => handleLikeMessage(msg)}
-                      className={`mr-2 text-blue-500 ${msg.likes && msg.likes.includes(user.uid) ? 'text-red-500' : ''}`}
-                    >
-                      {msg.likes && msg.likes.includes(user.uid) ? <FaThumbsDown /> : <FaThumbsUp />}
-                    </button>
-                    {user.uid === msg.userId && (
-                      <button
-                        onClick={() => handleDeleteMessage(msg)}
-                        className="text-blue-500"
-                      >
-                        <FaTrash />
-                      </button>
-                    )}
-                  </div>
+                  <span className="message-text">{msg.text}</span>
+                  {msg.timestamp && (
+                    <div className="message-time">
+                      {new Date(msg.timestamp.toDate()).toLocaleTimeString()}
+                    </div>
+                  )}
                 </div>
+                <div className="message-actions">
+                  <button
+                    onClick={() => handleLikeMessage(msg.id)}
+                    className={`like-button ${
+                      msg.likes && msg.likes.includes(user.uid) ? 'liked' : ''
+                    }`}
+                  >
+                    <FaHeart />
+                  </button>
+                  {user.uid === msg.userId && (
+                    <button
+                      onClick={() => handleDeleteMessage(msg)}
+                      className="delete-button"
+                    >
+                      <FaTrash />
+                    </button>
+                  )}
+                </div>
+                {msg.likes && msg.likes.length > 0 && (
+                  <div className="like-count">{msg.likes.length}</div>
+                )}
               </li>
             ))}
           </ul>
-          <form onSubmit={sendMessage} className="flex">
+          <form onSubmit={sendMessage} className="message-input">
             <input
               type="text"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
-              className="flex-grow border p-2 rounded-l focus:outline-none"
+              className="message-input-field"
               placeholder="Type your message..."
             />
-            <button
-              type="submit"
-              className="bg-blue-500 text-white px-4 py-2 rounded-r"
-            >
+            <button type="submit" className="send-button">
               Send
             </button>
           </form>
         </>
       ) : (
         <>
-          <p>Please login to chat</p>
-          <Link to="/" className="bg-blue-500 text-white px-4 py-2 rounded mt-4 inline-block">
+          <p className="login-prompt">Please login to chat</p>
+          <Link to="/" className="login-button">
             Login
           </Link>
         </>
@@ -147,3 +158,4 @@ const Chat = () => {
 };
 
 export default Chat;
+
