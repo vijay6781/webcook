8715eeb951edc2288ecd956501c6484a34568dd5
vijay kumar <64150328+vijay6781@users.noreply.github.com>
2023-09-27 23:37:03 +0530

    added customer chat and blog

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -8,7 +8,7 @@ function Navigation() {
   return (
     <nav className="flex flex-wrap items-center justify-center md:justify-end py-2.5 bg-gradient-to-r from-blue-300 to-blue-300">
       <a href="/" className="mr-4 mb-2 md:mb-0 text-black font-bold text-lg md:text-2xl">Home</a>
-      <a href="/About" className="mr-4 mb-2 md:mb-0 text-black font-bold text-lg md:text-2xl">About</a>
+      <a href="/blog" className="mr-4 mb-2 md:mb-0 text-black font-bold text-lg md:text-2xl">Blog</a>
       <SignIn/>
     </nav>
   );
