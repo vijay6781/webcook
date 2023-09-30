1ff8276e1e835810bbdef60d77911aec9798601c
vijay kumar <64150328+vijay6781@users.noreply.github.com>
2023-09-30 13:41:52 +0530

    added terms and condtions and refund policy

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -98,7 +98,7 @@ const Footer = () => {
         >
           Okay Result
         </a>
-        <div className="mt-2">
+        <div className="mt-0">
     <a href="/terms-and-conditions" className="text-blue-500 hover:text-blue-700 mx-2">Terms and Conditions</a>
     <a href="/privacy-policy" className="text-blue-500 hover:text-blue-700 mx-2">Privacy Policy</a>
   </div>
