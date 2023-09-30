41c3938e5cda3e06f87fd26736b57ab2e4c4f07e
vijay kumar <64150328+vijay6781@users.noreply.github.com>
2023-09-30 19:35:03 +0530

    chat support edited

--- src/components/ChatSupport/index.js
+++ src/components/ChatSupport/index.js
@@ -1,4 +1,3 @@
-/* ChatSupport.js */
 import React, { useState, useEffect } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
@@ -14,6 +13,7 @@ const ChatSupport = () => {
 
   const db = firebase.firestore();
   const usersRef = db.collection('users');
+  const messagesRef = db.collection('messages');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -30,7 +30,14 @@ const ChatSupport = () => {
   const handleMessageSubmit = async (e) => {
     e.preventDefault();
     if (!message) return;
-    await db.collection('messages').add({
+
+    const userMessages = messages.filter(msg => msg.name === '');
+    if (userMessages.length >= 10) {
+      alert('You have reached the message limit for this session.');
+      return;
+    }
+
+    await messagesRef.add({
       name,
       message,
       timestamp: new Date(),
@@ -39,7 +46,7 @@ const ChatSupport = () => {
   };
 
   useEffect(() => {
-    const unsubscribe = db.collection('messages').orderBy('timestamp').onSnapshot((snapshot) => {
+    const unsubscribe = db.collection('messages').where('name', '==', name).orderBy('timestamp').onSnapshot((snapshot) => {
       const newMessages = snapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
@@ -51,30 +58,29 @@ const ChatSupport = () => {
 
   return (
     <div className={`chat-support ${isOpen ? 'open' : ''}`}>
+      {!isOpen && !name && (
+        <button
+          className="chat-support-button bg-gradient-to-r from-cyan-400 to-light-blue-500"
+          onClick={() => setIsOpen(true)}
+        >
+          Chat Support
+        </button>
+      )}
       <div className="chat-support-content">
         <button className="chat-support-close" onClick={() => setIsOpen(false)}>
           &#10005;
         </button>
         <div className="chat-support-inner">
           <h2>Chat Support</h2>
-          <div className="messages" style={{ flexDirection: 'column-reverse' }}>
-            {messages.map((msg, index) => {
-              if (msg.name === name) {
-                return (
-                  <div key={index} className="message sent">
-                    <div className="message-content">{msg.message}</div>
-                  </div>
-                );
-              } else if (msg.name === 'Chat Bot') {
-                return (
-                  <div key={index} className="message received">
-                    <div className="message-content">{msg.message}</div>
-                  </div>
-                );
-              } else {
-                return null;
-              }
-            })}
+          <div className="messages">
+            {messages.map((msg, index) => (
+              <div
+                key={index}
+                className={`message ${msg.name === 'User' ? 'sent' : 'received'}`}
+              >
+                <div className="message-content">{msg.message}</div>
+              </div>
+            ))}
           </div>
           {isOpen && !name && (
             <form onSubmit={handleSubmit}>
